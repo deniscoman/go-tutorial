1cb8e08b85e31714c9096e7259cdeb5660b04cdf
deniscoman <denis.coman13@gmail.com>
2023-05-17 18:44:13 +0300

    Handle empty param for Hello func

--- hello.go
+++ hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const enlgishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return enlgishHelloPrefix + name
 }
 
 func main() {
